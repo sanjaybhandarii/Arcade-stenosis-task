f2429831dccff9ca6c0de82ba3ae55e126dcf08d
sanjaybhandarii <shawnjay001@gmail.com>
2023-10-07 09:40:09 +0545

    manual changes

--- mmdetection/train_sten.py
+++ mmdetection/train_sten.py
@@ -22,8 +22,7 @@ import numpy as np
 import random
 import os
 
-# os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-# os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
+
 
 
 cfg = Config.fromfile('/home/sbhandari/seg/mmdetection/configs/convnext/mask-rcnn_convnext-t-p4-w7_fpn_amp-ms-crop-3x_coco copy.py')
@@ -37,43 +36,32 @@ cfg.metainfo = {
 }
 
 
-# Modify dataset type and path
-cfg.data_root = '/home/sbhandari/combined_data'
+# Modify dataset type and path according to your dataset
+cfg.data_root = 'data'
 
-cfg.train_dataloader.dataset.ann_file = 'annotations/train.json'
+cfg.train_dataloader.dataset.ann_file = 'annotations/train.json'#path for ann_file for train dataset
 cfg.train_dataloader.dataset.data_root = cfg.data_root
-cfg.train_dataloader.dataset.data_prefix.img = 'images'
+cfg.train_dataloader.dataset.data_prefix.img = 'images'#trainset images
 cfg.train_dataloader.dataset.metainfo = cfg.metainfo
 cfg.train_dataloader.batch_size = 4
 cfg.train_dataloader.num_workers = 4
 
-cfg.val_dataloader.dataset.ann_file = 'annotations/valid.json'
+cfg.val_dataloader.dataset.ann_file = 'annotations/valid.json'#ann_file for validation set
 cfg.val_dataloader.dataset.data_root = cfg.data_root
-cfg.val_dataloader.dataset.data_prefix.img = 'images'
+cfg.val_dataloader.dataset.data_prefix.img = 'images' # valid set images
 cfg.val_dataloader.dataset.metainfo = cfg.metainfo
 cfg.val_dataloader.batch_size = 1
 cfg.val_dataloader.num_workers = 4
 
 cfg.test_dataloader = cfg.val_dataloader
-# cfg.train_cfg.max_epochs = 36
+
 
 
 # Modify metric config
 cfg.val_evaluator.ann_file = cfg.data_root+'/'+ 'annotations/valid.json'
-# cfg.test_evaluator.ann_file = cfg.data_root+'/'+ 'sten_val/annotations/test.json'
-# cfg.test_evaluator = cfg.val_evaluator
-#for mask-rcnn based
-# Modify num classes of the model in box head and mask head 
-#for sten 26 ,for seg 25
+
 cfg.model.roi_head.bbox_head.num_classes = 1
 cfg.model.roi_head.mask_head.num_classes = 1
-# cfg.model.roi_head.bbox_head.loss_bbox =dict(type='L1Loss', loss_weight=2.0)
-# cfg.model.roi_head.mask_head.loss_mask=dict(
-#                 type='CrossEntropyLoss', use_mask=True, loss_weight=2.0)
-# #for retinanet
-# cfg.model.bbox_head.num_classes = 1
-# cfg.model.mask_head.num_classes = 26
-
 
 
 # Set up working dir to save files and logs.
@@ -83,34 +71,27 @@ cfg.model.backbone.frozen_stages = 0
 
 # We can set the evaluation interval to reduce the evaluation times
 cfg.train_cfg.val_interval = 1
+
 # We can set the checkpoint saving interval to reduce the storage cost
 cfg.default_hooks.checkpoint.interval = 50
+
+#config for saving best model
 cfg.default_hooks.checkpoint.save_best = 'coco/segm_mAP_50'
 cfg.default_hooks.checkpoint.rule = 'greater'
 
-# Set seed thus the results are more reproducible
-# cfg.seed = 0
-# set_random_seed(0, deterministic=True)
 
-seed = 0
-random.seed(seed)
-np.random.seed(seed)
-torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
-# We can also use tensorboard to log the training process
-# cfg.visualizer.vis_backends.append({"type":'TensorboardVisBackend'})
 
+
+set_random_seed(0)
 
 
 #logging info into visbackend and log file
 cfg.default_hooks.logger.interval = 250
 
 #if manually downloaded the model use this
-# cfg.load_from = 'https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r101_fpn_1x_coco/mask_rcnn_r101_fpn_1x_coco_20200204-1efe0ed5.pth'
-cfg.load_from = 'https://download.openmmlab.com/mmdetection/v2.0/convnext/mask_rcnn_convnext-t_p4_w7_fpn_fp16_ms-crop_3x_coco/mask_rcnn_convnext-t_p4_w7_fpn_fp16_ms-crop_3x_coco_20220426_154953-050731f4.pth'
-# cfg.resume = True
+
+cfg.load_from = 'model.pth'
+
 
 # build the runner from config
 runner = Runner.from_cfg(cfg)
